b192d7649bf2bf155b4fd48f23d30777b368155b
tom-95 <tom.wehner95@web.de>
2021-04-30 16:52:40 +0200

    System.properties added

--- src/main/java/tom/wehner/advertisementWebapp/Controller.java
+++ src/main/java/tom/wehner/advertisementWebapp/Controller.java
@@ -1,12 +1,12 @@
 package tom.wehner.advertisementWebapp;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String sayHello() {
         return "Hello World!";
     }
