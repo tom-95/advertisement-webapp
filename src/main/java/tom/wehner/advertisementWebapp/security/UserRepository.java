66e376a20d79bd88731a7c7a03ebfdd7ff54dccb
tom-95 <tom.wehner95@web.de>
2022-05-24 19:22:27 +0200

    removed import

--- src/main/java/tom/wehner/advertisementWebapp/security/UserRepository.java
+++ src/main/java/tom/wehner/advertisementWebapp/security/UserRepository.java
@@ -2,7 +2,6 @@ package tom.wehner.advertisementWebapp.security;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import tom.wehner.advertisementWebapp.security.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
